c796525d5be2990e1757467b1767e60619b5127d
Hoang Viet Dung <vietdunghoang.hust@gmail.com>
2022-10-17 15:59:04 +0700

    init commit add vue-router, pinia, element-ui, axios test api

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1>{{ data }}</h1>
+  <el-button type="primary" icon="Menu">Submit</el-button>
+</template>
+
+<script setup>
+import axios from "@/request/axios.js";
+import { ref } from "vue";
+
+const data = ref("");
+
+const getData = async () => {
+  axios.get("/test").then((request) => {
+    console.log(request);
+    data.value = request.data;
+  });
+};
+
+getData();
+</script>
+
+<style scoped></style>
